5d16743d826b182a0fcbbae5d43a18da58791c06
nv284 <creativeuser284@gmail.com>
2024-03-24 18:31:31 +0530

    my-module-app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './admin/home/home.component';
+import { UserComponent } from './admin/user/user.component';
+import { PostComponent } from './admin/post/post.component';
+import { TravelComponent } from './data/travel/travel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'user',component:UserComponent},
+    {path:'post',component:PostComponent},
+    {path:'',component:HomeComponent},
+    {path:'travelData',component:TravelComponent},
+    
+];
